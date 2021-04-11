f3683be6cec4edecfa5a5180b4a88081cd1987d1
smadzelonka <smadzelonka@gmail.com>
2021-04-10 18:07:38 -0700

    changing file structure

---
+++ pages/categories/edit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const edit = () => {
+  return <div>Category Edit page</div>;
+};
+
+export default edit;
---
+++ pages/portfolios/edit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const edit = () => {
+  return <div>PORTFOLIO EDIT</div>;
+};
+
+export default edit;
